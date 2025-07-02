3708112bbf5c12d70449f017a53c50501daa804b
ReconceptX <reconcept1o@gmail.com>
2025-07-02 04:42:31 +0300

    efew

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import React from "react";
-import Home from "./components/Home"; // Mevcut anasayfa bileşeni
-import "./index.css"; // Global stiller (varsa)
+import { Routes, Route } from "react-router-dom"; // 1. Gerekli bileşenleri import et
+
+// Bileşenler
+import Home from "./components/Home";
 import WhatWeMade from "./components/WhatWeMade";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Bts from "./components/Bts";
-import HowItsWorks from "./components/HowItsWorks"
+import HowItsWorks from "./components/HowItsWorks";
 import Footer from "./components/Footer";
+import PrivacyPolicy from "./components/PrivacyPolicy"; // 2. PrivacyPolicy'yi import et
+
+// Ana sayfanın içeriğini bir araya getiren bir bileşen oluşturmak kodu temiz tutar.
+function MainPage() {
+  return (
+    <>
+      <Home />
+      <WhatWeMade />
+      <Bts />
+      <HowItsWorks />
+      <Footer />
+    </>
+  );
+}
+
+// Gizlilik politikası sayfasının düzeni
+function PrivacyPage() {
+  return (
+    <>
+      <PrivacyPolicy />
+      <Footer />{" "}
+      {/* Footer'ı burada da göstermek iyi bir kullanıcı deneyimi sağlar */}
+    </>
+  );
+}
 
 function App() {
   return (
     <div
       style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
     >
-      <Home />
-      <WhatWeMade />
+      {/* 3. Routes bileşeni ile rotaları tanımla */}
+      <Routes>
+        {/* Ana sayfa için rota: "/" URL'sinde MainPage bileşenini göster */}
+        <Route path="/" element={<MainPage />} />
 
-      <Bts />
-      <HowItsWorks />
-<Footer /> 
+        {/* Gizlilik Politikası için rota: "/imprint" URL'sinde PrivacyPage bileşenini göster */}
+        <Route path="/imprint" element={<PrivacyPage />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // 1. React Router'ı import et
+import App from "./App";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      {" "}
+      {/* 2. App bileşenini bununla sarmala */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
