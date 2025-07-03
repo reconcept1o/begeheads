c524f63634727266f1098496971b537b0cda82d6
ReconceptX <reconcept1o@gmail.com>
2025-07-03 03:30:04 +0300

    wegew

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom"; // 1. Gerekli bileşenleri import et
+import { Routes, Route } from "react-router-dom"; 
 
 // Bileşenler
 import Home from "./components/Home";
@@ -7,9 +7,9 @@ import WhatWeMade from "./components/WhatWeMade";
 import Bts from "./components/Bts";
 import HowItsWorks from "./components/HowItsWorks";
 import Footer from "./components/Footer";
-import PrivacyPolicy from "./components/PrivacyPolicy"; // 2. PrivacyPolicy'yi import et
+import PrivacyPolicy from "./components/PrivacyPolicy"; 
 
-// Ana sayfanın içeriğini bir araya getiren bir bileşen oluşturmak kodu temiz tutar.
+
 function MainPage() {
   return (
     <>
@@ -22,13 +22,13 @@ function MainPage() {
   );
 }
 
-// Gizlilik politikası sayfasının düzeni
+
 function PrivacyPage() {
   return (
     <>
       <PrivacyPolicy />
       <Footer />{" "}
-      {/* Footer'ı burada da göstermek iyi bir kullanıcı deneyimi sağlar */}
+     
     </>
   );
 }
@@ -38,12 +38,12 @@ function App() {
     <div
       style={{ minHeight: "100vh", display: "flex", flexDirection: "column" }}
     >
-      {/* 3. Routes bileşeni ile rotaları tanımla */}
+      
       <Routes>
-        {/* Ana sayfa için rota: "/" URL'sinde MainPage bileşenini göster */}
+        
         <Route path="/" element={<MainPage />} />
 
-        {/* Gizlilik Politikası için rota: "/imprint" URL'sinde PrivacyPage bileşenini göster */}
+        
         <Route path="/privacy-policy" element={<PrivacyPage />} />
       </Routes>
     </div>
