b99de401f12094ae579838345bcc9fe255d11ff7
ReconceptX <reconcept1o@gmail.com>
2025-07-03 04:31:21 +0300

    fewgew

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -96,7 +96,7 @@ function Home() {
   // --- STYLES (Değişiklik yok) ---
   const rootStyle = {
     position: "relative",
-    width: "100vw",
+    width: "100%",
     height: "100vh",
     backgroundColor: "#141414",
     backgroundImage: `url(${backgroundUrl})`,
